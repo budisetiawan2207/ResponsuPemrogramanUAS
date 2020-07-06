766009406ff54ccd2e197c30d82437b5986e6588
budisetiawan2207 <budi.2207@students.amikom.ac.id>
2020-07-06 18:56:18 +0700

    commit

---
+++ Produk.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProjectProduk
+{
+    public class Produk
+    {
+        // PERINTAH: lengkapi property class produk, sesuai petunjuk soal
+        public string kodeProduk { get; set; }
+        public string namaProduk { get; set; }
+        public double hargaBeli { get; set; }
+        public double hargaJual { get; set; }
+    }
+}
